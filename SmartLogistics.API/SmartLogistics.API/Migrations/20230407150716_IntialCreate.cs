148b9681e8c3d45fb7cdd96ea3ddd2dff2633304
Markus Staub <markusstaub1@gmail.com>
2023-04-07 17:56:06 +0200

    initial create database

---
+++ SmartLogistics.API/SmartLogistics.API/Migrations/20230407150716_IntialCreate.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartLogistics.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bestellungen",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Erfassdatum = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LieferungStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LieferungEnde = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    KundenId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bestellungen", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Geschlechter",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Beschreibung = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Geschlechter", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lieferungen",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Priorität = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Lieferart = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BestellId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lieferungen", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orte",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Ortschaft = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Postleitzahl = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orte", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produkte",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produkte", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roboter",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roboter", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kunden",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Vorname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nachname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Geburtsdatum = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefon = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GeschlechtId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kunden", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Kunden_Geschlechter_GeschlechtId",
+                        column: x => x.GeschlechtId,
+                        principalTable: "Geschlechter",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Adressen",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Strasse = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Hausnummer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrtId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    KundeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adressen", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Adressen_Kunden_KundeId",
+                        column: x => x.KundeId,
+                        principalTable: "Kunden",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adressen_KundeId",
+                table: "Adressen",
+                column: "KundeId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kunden_GeschlechtId",
+                table: "Kunden",
+                column: "GeschlechtId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adressen");
+
+            migrationBuilder.DropTable(
+                name: "Bestellungen");
+
+            migrationBuilder.DropTable(
+                name: "Lieferungen");
+
+            migrationBuilder.DropTable(
+                name: "Orte");
+
+            migrationBuilder.DropTable(
+                name: "Produkte");
+
+            migrationBuilder.DropTable(
+                name: "Roboter");
+
+            migrationBuilder.DropTable(
+                name: "Kunden");
+
+            migrationBuilder.DropTable(
+                name: "Geschlechter");
+        }
+    }
+}
--- SmartLogistics.API/SmartLogistics.API/Program.cs
+++ SmartLogistics.API/SmartLogistics.API/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SmartLogistics.API.DataModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<SmartLogisticsContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("SmartLogisticsAdminPortalDb")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
