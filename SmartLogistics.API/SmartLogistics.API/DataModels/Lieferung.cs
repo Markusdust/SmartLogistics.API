5f3004560d2cf5535446a2cc82b768bf0e90d468
Markus Staub <markusstaub1@gmail.com>
2023-04-07 15:16:17 +0200

    define DBset in DbContext

--- SmartLogistics.API/SmartLogistics.API/DataModels/Lieferung.cs
+++ SmartLogistics.API/SmartLogistics.API/DataModels/Lieferung.cs
@@ -1,11 +1,11 @@
-﻿namespace SmartLogistics.API
+﻿namespace SmartLogistics.API.DataModels
 {
     public class Lieferung
     {
         public Guid Id { get; set; }
         public string Status { get; set; }
         public string Priorität { get; set; }
-        public string Lieferart { get; set; } 
+        public string Lieferart { get; set; }
         public Guid BestellId { get; set; }
     }
 }
--- SmartLogistics.API/SmartLogistics.API/DataModels/SmartLogisticsContext.cs
+++ SmartLogistics.API/SmartLogistics.API/DataModels/SmartLogisticsContext.cs
@@ -8,5 +8,14 @@ namespace SmartLogistics.API.DataModels
         {
             
         }
+
+        public DbSet<Kunde> Kunden { get; set; }
+        public DbSet<Geschlecht> Geschlechter { get; set; }
+        public DbSet<Adresse> Adressen { get; set; }
+        public DbSet<Ort> Orte { get; set; }
+        public DbSet<Bestellung> Bestellungen { get; set; }
+        public DbSet<Lieferung> Lieferungen { get; set; }
+        public DbSet<Produkt> Produkte { get; set; }
+        public DbSet<Roboter> Roboter { get; set; }
     }
 }
