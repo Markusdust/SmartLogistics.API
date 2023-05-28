6277149c08ea62264dcb18a1aed5dda6c4241142
Markusdust <76920344+Markusdust@users.noreply.github.com>
2023-05-28 13:56:53 +0200

    creating Bestellung Interface

---
+++ SmartLogistics.API/SmartLogistics.API/Repositories/IBestellungRepository.cs
@@ -0,0 +1,18 @@
+﻿using SmartLogistics.API.DataModels;
+
+namespace SmartLogistics.API.Repositories
+{
+    public interface IBestellungRepository
+    {
+        Task<List<Bestellung>> GetBestellungenAsync();
+
+        Task<Bestellung> GetBestellungAsync(Guid bestellungId);
+
+        Task<bool> Exists(Guid bestellungId);
+
+        Task<Bestellung> UpdateBestellung(Guid kundenId, Bestellung request);
+
+        Task<Bestellung> DeleteBestellung(Guid bestellungId);
+        Task<Bestellung> AddBestellung(Bestellung request);
+    }
+}
---
+++ SmartLogistics.API/SmartLogistics.API/Repositories/SqlBestellungRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartLogistics.API.DataModels;
+
+namespace SmartLogistics.API.Repositories
+{
+    public class SqlBestellungRepository : IBestellungRepository
+    {
+        private readonly SmartLogisticsContext context;
+
+        public SqlBestellungRepository(SmartLogisticsContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Bestellung>> GetBestellungenAsync()
+        {
+            return await context.Bestellungen.Include(nameof(Geschlecht)).Include(nameof(Adresse)).ToListAsync();
+        }
+
+        public async Task<Bestellung> GetBestellungAsync(Guid bestellungId)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<bool> Exists(Guid bestellungId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Bestellung> UpdateBestellung(Guid kundenId, Bestellung request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Bestellung> DeleteBestellung(Guid bestellungId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Bestellung> AddBestellung(Bestellung request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
